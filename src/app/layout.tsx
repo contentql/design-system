5667f067ec22d0a9cdae6d3ff24efeefc40d2696
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-09-08 15:02:45 +0530

    favicon: updating favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,24 +21,24 @@ export const metadata = {
   icons: [
     {
       rel: 'icon',
-      url: '/favicon/favicon.ico',
+      url: '/favicon/favicon.png',
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '16x16',
-      url: '/favicon/favicon-16x16.png',
+      url: '/favicon/favicon.png',
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '32x32',
-      url: '/favicon/favicon-32x32.png',
+      url: '/favicon/favicon.png',
     },
     {
       rel: 'apple-touch-icon',
       sizes: '180x180',
-      url: '/favicon/apple-touch-icon.png',
+      url: '/favicon/favicon.png',
     },
   ],
 };
